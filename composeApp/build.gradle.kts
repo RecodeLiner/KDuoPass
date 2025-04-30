0b0034c29ea8c236819ea5026a33bab3232be223
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2025-04-30 19:25:00 +0300

    feat: Add extended Material Icons to Compose dependencies
    This commit adds the `materialIconsExtended` library to the Compose dependencies. This enables the use of a wider range of Material Design icons within the application.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -34,6 +34,7 @@ kotlin {
                 implementation(compose.runtime)
                 implementation(compose.foundation)
                 implementation(compose.material3)
+                implementation(compose.materialIconsExtended)
                 implementation(compose.components.uiToolingPreview)
                 implementation(compose.components.resources)
                 implementation(libs.kotlinx.coroutines.core)
